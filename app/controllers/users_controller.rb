268734b2be030a95a894fc390ac176d0de744d4b
naka5419 <hnakai323@yahoo.co.jp>
2019-04-06 14:36:13 +0900

    Add incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+  def index
+
+    @users = User.where('name LIKE(?) and id != ?', "#{params[:keyword]}%", current_user.id)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+   end
+  end
+
   def edit
   end
 
---
+++ app/views/users/index.json.jbuilder
@@ -0,0 +1,4 @@
+json.array! @users do |user|
+  json.id @uer.id
+  json.name @user.name
+end
