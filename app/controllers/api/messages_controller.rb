d91c6bf17597c9de50a1fd1de6b30e76b38c801a
naka5419 <hnakai323@yahoo.co.jp>
2019-04-10 16:16:44 +0900

    Edit- indent

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,13 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-  	#@messages = @group.messages.where('id > ?', params[:id])
-  	#@message = @messages.where('id > ?', params[:id])
-
-  	@messages = Message.where('id > ?', params[:id])
-  	 #@new_message = @messages.where('id > ?', params[:last_id])
-	#@messages = messages.where('id > ?', params[:id])
-	#@messages = @group.messages.includes(:user).where('id > ?', params[:id])
-	#messages = Message.where('id > ?', params[:id])
-	#@messages = @messages.where('id > ?', params[:last_id])
+    @messages = Message.where('id > ?', params[:id])
   end
 end
